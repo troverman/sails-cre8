11b380d6edff55bc44b1d4662e178c1d56c2b84f
Trevor Overman <troverman@gmail.com>
2015-07-13 14:50:13 -0400

    hello

--- assets/src/app/app.js
+++ assets/src/app/app.js
@@ -17,16 +17,39 @@ angular.module( 'sailng', [
 ])
 
 .config( function myAppConfig ( $stateProvider, $urlRouterProvider, $locationProvider ) {
-    $urlRouterProvider.otherwise(function ($injector, $location) {
+    //$urlRouterProvider.otherwise(function ($injector, $location) {
         //if ($location.$$url === '/') {
             //window.location = '/home';
         //}
         //else {
             // pass through to let the web server handle this request
-            window.location = $location.$$absUrl;
+            //window.location = $location.$$absUrl;
         //}
+    //});
+    $routeProvider.when("/", {
+        templateUrl: "/home/index.tpl.html",
+        controller: "HomeCtrl"
+    }).when("/about", {
+        templateUrl: "/about/index.tpl.html",
+        controller: "AboutCtrl"
+    }).when("/messages", {
+        templateUrl: "/messages/index.tpl.html",
+        controller: "MessagesController"
+    }).when("/member", {
+        templateUrl: "/member/index.tpl.html",
+        controller: "MemberCtrl"
+    }).otherwise({
+        redirectTo: "/"
     });
     $locationProvider.html5Mode(true);
+
+
+
+
+
+
+
+
 })
 
 .run( function run () {
