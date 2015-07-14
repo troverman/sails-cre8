7f58d3a4ea549f0e3f2223606dce1d91c1a04b68
Trevor Overman <troverman@gmail.com>
2015-07-14 01:01:58 -0400

    ok

--- assets/src/app/member/index.js
+++ assets/src/app/member/index.js
@@ -3,7 +3,13 @@ angular.module( 'sailng.member', [
 
 .config(function config( $stateProvider ) {
 	$stateProvider.state( 'member', {
-		url: '/member',
+		url: '/member/:memberID/:memberPage',
+        controller: function($scope, $stateParams) {
+            // get the id
+            $scope.id = $stateParams.memberID;
+            $scope.location = $stateParams.memberPage; 
+              
+        },
 		views: {
 			"main": {
 				controller: 'MemberCtrl',
