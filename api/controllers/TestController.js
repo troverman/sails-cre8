9f98cecc34755fc8cf972714d59ea386af31b471
Trevor Overman <troverman@gmail.com>
2015-07-21 22:04:55 -0400

    yo

--- api/controllers/TestController.js
+++ api/controllers/TestController.js
@@ -6,18 +6,22 @@ module.exports = {
             {whatup: 'hello!', mhm: 'awww-yis', soo: '1234'}
         ];
 
-        /*User.findAll().exec(function(err, users) {
+        User.findAll().exec(function(err, users) {
+
+        	if (err) throw err;
+
             res.view({
                 members: users
             });
-        });*/
+            
+        });
 
-		User.getAll()
-		.exec(function(err, models) {
-			res.view({
-				members: models
-			});
-		});
+		//User.getAll()
+		//.exec(function(err, models) {
+			//res.view({
+				//members: models
+			//});
+		//});
         //res.view({
             //testItems: testItems
         //});
