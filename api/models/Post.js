1d19277ece1fe42030b83e075cd707f4121080c7
Trevor Overman <troverman@gmail.com>
2015-07-22 04:21:56 -0400

    yo

--- api/models/Post.js
+++ api/models/Post.js
@@ -7,8 +7,43 @@
 
 module.exports = {
 
-  attributes: {
+	attributes: {
+        member_id: {
+            type: 'string',
+            required: true,
+            unique: true
+        },
+        title: {
+            type: 'string',
+            required: true,
+            unique: true
+        },
+        post_content: {
+            type: 'string',
+            required: true
+        },
+        other: {
+            type: 'string'
+        },
+        url_title: {
+            type: 'string',
+            required: true,
+            unique: true
+        }
+    },
 
-  }
+    getAll: function() {
+        return User.find()
+        .then(function (models) {
+            return [models];
+        });
+    },
+
+    getOne: function(id) {
+        return User.findOne(id)
+        .then(function (model) {
+            return [model];
+        });
+    }
 };
 
