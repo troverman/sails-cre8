c1c368c8fa260188a8a593a4f032bf1c343e64b4
Trevor Overman <troverman@gmail.com>
2015-07-21 23:54:56 -0400

    yo

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -10,7 +10,7 @@ module.exports = {
 	},
 
 	getOne: function(req, res) {
-		User.getOne(req.param('id'))
+		User.getOne(req.param('username'))
 		.spread(function(model) {
 			res.json(model);
 		})
