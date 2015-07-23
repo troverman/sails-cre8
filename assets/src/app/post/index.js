ec5190eb419f6e4c7c49e1a8007fc7ed15199b23
Trevor Overman <troverman@gmail.com>
2015-07-22 20:51:29 -0400

    hello

--- assets/src/app/post/index.js
+++ assets/src/app/post/index.js
@@ -18,10 +18,34 @@ angular.module( 'sailng.post', [
 	});
 })
 
-.controller( 'PostCtrl', function PostController( $scope, titleService ) {
+.controller( 'PostCtrl', function PostController( $scope, titleService, posts, PostModel) {
 	titleService.setTitle('post');
+	$scope.newPost = {};
+    $scope.posts = posts;
+
+
+    $sailsSocket.subscribe('post', function (envelope) {
+	    switch(envelope.verb) {
+	        case 'created':
+	            $scope.posts.unshift(envelope.data);
+	            break;
+	        case 'destroyed':
+	            lodash.remove($scope.posts, {id: envelope.id});
+	            break;
+	    }
+    });
+
+
+
 
 	$scope.master = {};
+
+	$scope.createPost = function(newMessage) {
+        newPost.user = config.currentUser.id;
+        PostModel.create(newPost).then(function(model) {
+            $scope.newPost = {};
+        });
+    };
 
 	$scope.update = function(post) {
 		$scope.master = angular.copy(post);
