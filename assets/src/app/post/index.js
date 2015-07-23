0a8cf39beecf312e65d70b42db63d7e75cefea89
Trevor Overman <troverman@gmail.com>
2015-07-23 02:26:56 -0400

    hello

--- assets/src/app/post/index.js
+++ assets/src/app/post/index.js
@@ -11,7 +11,7 @@ angular.module( 'sailng.post', [
 			}
 		},
 		resolve: {
-            posts: function(PostModel) {
+            messages: function(PostModel) {
                 return PostModel.getAll().then(function(models) {
                     return models;
                 });
@@ -20,7 +20,7 @@ angular.module( 'sailng.post', [
 	});
 })
 
-.controller( 'PostCtrl', function PostController( $scope, $sailsSocket, lodash, titleService, config, PostModel, posts) {
+.controller( 'PostCtrl', function PostController( $scope, $sailsSocket, lodash, titleService, config, PostModel, messages) {
 	titleService.setTitle('post');
 
     $scope.currentUser = config.currentUser;
