dbf8c3635af6d4aa43c32b531cbdfb7fb85f2885
Trevor Overman <troverman@gmail.com>
2015-07-23 01:56:35 -0400

    hello

--- assets/src/app/post/index.js
+++ assets/src/app/post/index.js
@@ -20,13 +20,13 @@ angular.module( 'sailng.post', [
 	});
 })
 
-.controller( 'PostCtrl', function PostController( $scope, titleService, config, PostModel) {
+.controller( 'PostCtrl', function PostController( $scope, titleService, config, PostModel, posts) {
 	titleService.setTitle('post');
 
     $scope.currentUser = config.currentUser;
 	$scope.master = {};
 	$scope.newPost = {};
-    //$scope.posts = posts;
+    $scope.posts = posts;
 
     /*$sailsSocket.subscribe('post', function (envelope) {
 	    switch(envelope.verb) {
