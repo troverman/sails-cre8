ad9028d2c152577702133fae1f24d839b03cf90f
Trevor Overman <troverman@gmail.com>
2015-07-23 03:23:38 -0400

    hello

--- assets/src/app/post/index.js
+++ assets/src/app/post/index.js
@@ -1,4 +1,4 @@
-/*angular.module( 'sailng.post', [
+angular.module( 'sailng.post', [
 ])
 
 .config(function config( $stateProvider ) {
@@ -40,60 +40,16 @@
         });
     };
 
-});*/
-
-
-angular.module( 'sailng.post', [
-])
-
-.config(function config( $stateProvider ) {
-    $stateProvider.state( 'post', {
-        url: '/post',
-        views: {
-            "main": {
-                controller: 'PostCtrl',
-                templateUrl: 'post/index.tpl.html'
-            }
-        },
-        resolve: {
-            posts: function(postModel) {
-                return postModel.getAll().then(function(models) {
-                    return models;
-                });
-            }
-        }
-    });
-})
-
-.controller( 'PostCtrl', function PostController( $scope, titleService, postModel, posts ) {
-    titleService.setTitle('posts');
-    $scope.newPost = {};
-    /*$scope.currentUser = config.currentUser;
-
-    $sailsSocket.subscribe('post', function (envelope) {
-        switch(envelope.verb) {
-            case 'created':
-                $scope.posts.unshift(envelope.data);
-                break;
-            case 'destroyed':
-                lodash.remove($scope.posts, {id: envelope.id});
-                break;
-        }
-    });
-
-    $scope.destroyPost = function(post) {
-        // check here if this post belongs to the currentUser
-        if (post.user.id === config.currentUser.id) {
-            postModel.delete(post).then(function(model) {
-                // post has been deleted, and removed from $scope.posts
-            });
-        }
-    };
-
-    $scope.createPost = function(newPost) {
-        newPost.user = config.currentUser.id;
-        postModel.create(newPost).then(function(model) {
-            $scope.newPost = {};
-        });
-    };*/
 });
+
+
+
+
+
+
+
+
+
+
+
+
